b70626d9aeae2260f73197cea8230fbfce7dce2b
miaoshihu <miaoshihu@gmail.com>
2019-11-03 21:55:45 +0800

    diplay chosed picture

--- pages/new/new.js
+++ pages/new/new.js
@@ -12,22 +12,38 @@ Page({
     image_path_2: "../../imgs/add.jpeg"
   },
 
-  bindInputName: function (e) {
+  bindInputName: function(e) {
     log("Page-new bindInputName e " + e.detail.value);
   },
 
-  formSubmit: function (e) {
+  formSubmit: function(e) {
     log('Page-new form发生了submit事件，携带数据为hh：', e)
   },
 
-  formReset: function () {
+  formReset: function() {
     log('Page-new form发生了reset事件')
   },
 
-  onLoad: function (options) {
+  addImage1: function(e) {
+    log("addImage1 " + e);
+    var _this = this;
+    wx.chooseImage({
+      count: 1, //最多可以选择的图片总数
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'],
+      success: function(res) {
+        var tempPath = res.tempFilePaths[0];
+        _this.setData({
+          image_path_1: tempPath,
+        });
+      }
+    });
+  },
+
+  onLoad: function(options) {
     log("Page-new onLoad");
   },
-  onShow: function (options) {
+  onShow: function(options) {
     log("Page-new onShow");
   },
 })
