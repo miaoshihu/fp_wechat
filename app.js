2dff1fb76a05bc936da7725f887cac28deef6ea9
miaoshihu <miaoshihu@gmail.com>
2019-11-04 23:22:02 +0800

    add wx.login code

--- app.js
+++ app.js
@@ -8,36 +8,33 @@ const log = require('./utils/log').log_app
 App({
   onLaunch: function (options) {
 
-    log("App() onLaunch options.path = " + options.path);
-    log("App() onLaunch options.query = " + options.query);
-    log("App() onLaunch options.scene = " + options.scene);
-    log("App() onLaunch options.shareTicket = " + options.shareTicket);
+    log("onLaunch options.path = " + options.path);
+    log("onLaunch options.query = " + options.query);
+    log("onLaunch options.scene = " + options.scene);
+    log("onLaunch options.shareTicket = " + options.shareTicket);
 
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-
-    // wx.login({
-    //   success: function (res) {
-
-    //     wx.request({
-    //       url: wechatLoginUrl,
-    //       data: {
-    //         code: res.code,
-    //         app_id: appId,
-    //         app_secret: appSecret
-    //       },
-    //       success: function (res) {
-    //         console.log("wx.request fps server success ***********")     
-    //         console.log("res = " + res.data)
-    //         console.log(res.statusCode)      
-    //       }
-    //     })
-
-    //   }
-    // })
+    log("onLaunch -> wx.login");
+    wx.login({
+      success: function (res) {
+        log("onLaunch -> wx.login success -> wx.request self server");
+        wx.request({
+          url: wechatLoginUrl,
+          data: {
+            code: res.code,
+            app_id: appId,
+            app_secret: appSecret
+          },
+          success: function (res) {
+            log("onLaunch -> wx.login self server success ");
+            console.log("res = " + res.data)
+            console.log(res.statusCode)      
+          },
+          fail: function (res) {
+            log("onLaunch -> wx.login self server fail ");
+          }
+        })
+      }
+    })
   },
 
   globalData: {
